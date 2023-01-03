1e416dbe5f1e6aeac8023f641d3533575919ea23
MindCrawler099 <botslayer466@gmail.com>
2023-01-03 17:37:18 +0000

    Gave the player controller basic look
    The player camera can now look vertically

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,14 +7,20 @@ public class PlayerController : MonoBehaviour
     [Header("PlayerControllerConfig")]
     [SerializeField] private PlayerControllerConfig playerControllerConfig;
 
+    [Header("Assigned Transforms")]
+    [SerializeField] private Transform fpCamera;
+
     //Referenced Variables
     private CharacterController characterController;
     private PlayerInput playerInput;
 
     //Private Variables
+    //Movement
     private float currentPlayerSpeed = 5f;
     private Vector2 playerInputVector  = Vector3.zero;
     private Vector3 movementVector = Vector3.zero;
+    //Look
+    private float rotationX = 0f;
 
     private void Awake()
     {
@@ -25,6 +31,7 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         PlayerMovement();
+        PlayerLook();
     }
 
     private void PlayerMovement()
@@ -32,5 +39,11 @@ public class PlayerController : MonoBehaviour
         playerInputVector = playerInput.actions["Move"].ReadValue<Vector2>();
         movementVector = ((transform.forward * playerInputVector.y) + (transform.right * playerInputVector.x)) * currentPlayerSpeed;
         characterController.Move(movementVector * Time.deltaTime);
+    }
+
+    private void PlayerLook()
+    {
+        rotationX += playerControllerConfig.isLookInvertedX * playerInput.actions["Look"].ReadValue<Vector2>().y * playerControllerConfig.lookSensitivity * Time.deltaTime;
+        fpCamera.rotation = Quaternion.Euler(new Vector3(rotationX, 0f, 0f));
     }
 }
--- Assets/Scripts/Player/PlayerControllerConfig.cs
+++ Assets/Scripts/Player/PlayerControllerConfig.cs
@@ -5,4 +5,36 @@ public class PlayerControllerConfig : ScriptableObject{
     [Header("Movement Parameters")]
     public float walkSpeed = 5f;
     public float runSpeed = 10f;
+
+    [Header("Look Parameters")]
+    [Range(1f, 200f)] public float lookSensitivity = 100f;
+    [SerializeField] private bool lookInvertedX = false;
+    [SerializeField] private bool lookInvertedY = false;
+
+    public float isLookInvertedX
+    {
+        get
+        {
+            if (lookInvertedX == false)
+            {
+                return -1f;
+            }
+            else
+            {
+                return 1f;
+            }
+        }
+    }
+    public float isLookInvertedY
+    {
+        get
+        {
+            if (lookInvertedY == false){
+                return -1f;
+            }
+            else{
+                return 1f;
+            }
+        }
+    }
 }
